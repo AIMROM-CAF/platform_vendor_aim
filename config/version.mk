orchidnpsg/vendor_aim

aim: Restore ro.aim.build.version
* For use with Updater.

Signed-off-by: Albert I <kras@raphielgang.org>

config/version.mk
@@ -16,23 +16,25 @@
 
 PRODUCT_BRAND ?= AIMROM
 
-AIM_BASE_VERSION = System-V4.0
+AIM_BASE_NAME = System
+AIM_BASE_VERSION = V4.0
 
 ifndef AIM_BUILD_TYPE
     AIM_BUILD_TYPE := Unofficial
 endif
 
 # Set all versions
-AIM_VERSION := AIM-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)-$(AIM_BUILD)-$(shell date -u +%Y%m%d-%H%M)
+AIM_VERSION := AIM-$(AIM_BASE_NAME)-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)-$(AIM_BUILD)-$(shell date -u +%Y%m%d-%H%M)
 
-AIM_DISPLAY_VERSION := AIM-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)
+AIM_DISPLAY_VERSION := AIM-$(AIM_BASE_NAME)-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)
 
-AIM_MOD_VERSION := $(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)
+AIM_MOD_VERSION := $(AIM_BASE_NAME)-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)
 
 # Overrides
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
     ro.aim.version=$(AIM_VERSION) \
+    ro.aim.build.version=$(AIM_BASE_VERSION) \
     ro.mod.version=$(AIM_MOD_VERSION)
 
 # Display version

AIMROM-CAF/platform_vendor_aim

Change-Id: I467a31811eb5d6ac02f9386bd05262dc26fbfb49

config/version.mk
@@ -16,23 +16,25 @@
 
 PRODUCT_BRAND ?= AIMROM
 
-AIM_BASE_VERSION = System-V5.0
+AIM_BASE_NAME = System
+AIM_BASE_VERSION = V5.0
 
 ifndef AIM_BUILD_TYPE
     AIM_BUILD_TYPE := Unofficial
 endif
 
 # Set all versions
-AIM_VERSION := AIM-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)-$(AIM_BUILD)-$(shell date -u +%Y%m%d-%H%M)
+AIM_VERSION := AIM-$(AIM_BASE_NAME)-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)-$(AIM_BUILD)-$(shell date -u +%Y%m%d-%H%M)
 
-AIM_DISPLAY_VERSION := AIM-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)
+AIM_DISPLAY_VERSION := AIM-$(AIM_BASE_NAME)-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)
 
-AIM_MOD_VERSION := $(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)
+AIM_MOD_VERSION := $(AIM_BASE_NAME)-$(AIM_BASE_VERSION)-$(AIM_BUILD_TYPE)
 
 # Overrides
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
     ro.aim.version=$(AIM_VERSION) \
+    ro.aim.build.version=$(AIM_BASE_VERSION) \
     ro.mod.version=$(AIM_MOD_VERSION)
 
 # Display version